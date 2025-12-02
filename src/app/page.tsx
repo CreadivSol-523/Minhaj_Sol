9cfd92cb0184411482439fa6f7f1a87f0f988b47
wasif903 <wasifmehmood903@gmail.com>
2025-12-03 04:21:20 +0500

    Chnages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import LandingPage from "./components/LandingPage";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <h1 className="text-red-500">test</h1>
+      <LandingPage />
     </div>
   );
 }
