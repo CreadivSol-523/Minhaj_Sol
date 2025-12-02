b40a6c44defaa99916714598c5449e1d1355fad3
hassan523 <hs542327@gmail.com>
2025-12-03 01:28:23 +0500

    push

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <h1 className="text-red-500">test</h1>
+    </div>
+  );
+}
