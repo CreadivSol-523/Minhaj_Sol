5c53c180b28bcb9769f956ae8f999c89e45b897f
hasan8733 <imtiazhasan873@gmail.com>
2025-12-07 22:53:46 +0500

    push

--- src/components/LandingPageComp/OurStackSection/OurStackSection.tsx
+++ src/components/LandingPageComp/OurStackSection/OurStackSection.tsx
@@ -1,25 +1,44 @@
+"use client";
 import Image from "next/image";
+import Marquee from "react-fast-marquee";
 
 const OurStackSection = () => {
+  const techImages = [
+    "/Images/home/Stacks/TechIcon1.png",
+    "/Images/home/Stacks/TechIcon3.png",
+    "/Images/home/Stacks/TechIcon4.png",
+    "/Images/home/Stacks/TechIcon5.png",
+    "/Images/home/Stacks/TechIcon7.png",
+    "/Images/home/Stacks/TechIcon8.png",
+    "/Images/home/Stacks/TechIcon9.png",
+  ];
+
   return (
-    <section className=" bg-white">
+    <section className="bg-white">
       <div className="flex flex-col md:gap-20 gap-5 2xl:px-80 lg:px-40 px-10">
         <h3 className="text-center">Our Tech Stack</h3>
 
-        <div className="grid 2xl:grid-cols-6 lg:grid-cols-4 grid-cols-3 gap-4 sm:gap-6 lg:gap-8 place-items-center space-y-16">
-          <Image src={"/Images/home/Stacks/TechIcon1.png"} className="md:w-24 sm:w-16 w-10" alt="Icons" width={100} height={100} />
-          <Image src={"/Images/home/Stacks/TechIcon3.png"} className="md:w-24 sm:w-16 w-10" alt="Icons" width={100} height={100} />
-          <Image src={"/Images/home/Stacks/TechIcon4.png"} className="md:w-24 sm:w-16 w-10" alt="Icons" width={100} height={100} />
-          <Image src={"/Images/home/Stacks/TechIcon5.png"} className="md:w-24 sm:w-16 w-10" alt="Icons" width={100} height={100} />
-          <Image src={"/Images/home/Stacks/TechIcon2.png"} className="md:w-24 sm:w-16 w-10" alt="Icons" width={100} height={100} />
-          <Image src={"/Images/home/Stacks/TechIcon7.png"} className="md:w-24 sm:w-16 w-10" alt="Icons" width={100} height={100} />
-          <Image src={"/Images/home/Stacks/TechIcon7.png"} className="md:w-24 sm:w-16 w-10" alt="Icons" width={100} height={100} />
-          <Image src={"/Images/home/Stacks/TechIcon8.png"} className="md:w-24 sm:w-16 w-10" alt="Icons" width={100} height={100} />
-          <Image src={"/Images/home/Stacks/TechIcon9.png"} className="md:w-24 sm:w-16 w-10" alt="Icons" width={100} height={100} />
-          <Image src={"/Images/home/Stacks/TechIcon1.png"} className="md:w-24 sm:w-16 w-10" alt="Icons" width={100} height={100} />
-          <Image src={"/Images/home/Stacks/TechIcon4.png"} className="md:w-24 sm:w-16 w-10" alt="Icons" width={100} height={100} />
-          <Image src={"/Images/home/Stacks/TechIcon5.png"} className="md:w-24 sm:w-16 w-10" alt="Icons" width={100} height={100} />
-        </div>
+        <Marquee
+          speed={60}
+          gradient={false}
+          pauseOnHover={true}
+          className="flex items-center"
+        >
+          {[...techImages, ...techImages].map((src, index) => (
+            <div
+              key={index}
+              className="xl:mx-20 lg:mx-14 sm:mx-8 mx-6 flex justify-center items-center"
+            >
+              <Image
+                src={src}
+                alt={`Tech Icon ${index}`}
+                width={100}
+                height={100}
+                className="md:w-24 sm:w-16 w-10 object-contain"
+              />
+            </div>
+          ))}
+        </Marquee>
       </div>
     </section>
   );
